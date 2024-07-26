bb97e07b3b97fb267ee8ed97e245d804c9682fbd
denizokan.taspinar <denizokan.taspina@arcelik.com>
2024-07-26 03:07:40 +0300

    string ve date konusu bitimi

---
+++ StringsAndDates/StringUygulamalari.cs
@@ -0,0 +1,12 @@
+/*
+String Uygulamaları
+*/
+
+// string kursAdi = ".NET ile C# programlama dili";
+// Console.WriteLine(kursAdi);
+
+// //Dili yerine Dersleri yazdırma
+
+// var guncellenenString = kursAdi.Replace("dili","dersleri");
+
+// Console.WriteLine(guncellenenString);
