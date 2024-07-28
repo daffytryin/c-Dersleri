78c2243ca1ef63b83232c422316759fd698f4643
denizokan.taspinar <denizokan.taspina@arcelik.com>
2024-07-28 21:35:52 +0300

    operatörler için yeni bir branch oluşturuldu

---
+++ Operatorler/KarsilastirmaOperatorleri.cs
@@ -0,0 +1,28 @@
+/*
+
+KARŞILAŞTIRMA OPERATÖRLERİ
+
+Klavyeden girilen yaş bilgisine göre oy kullanabilme durumunu incele
+Girilen bir sayının işaretini kontrol ediniz
+Girilen bir sayının tek çift olma durumunu kontrol ediniz
+
+*/
+
+// Console.Write("Yaşınız: ");
+
+// var yas = Convert.ToInt32(Console.ReadLine());
+
+// var sonuc = (yas >= 18) ? "ehliyet alabilirsiniz":"ehliyet için yaşınız tutmuyor";
+
+// Console.WriteLine($"Ehliyet alabilme durumu: {sonuc}");
+
+// Console.WriteLine("Sayı:");
+// var sayi = Convert.ToInt32(Console.ReadLine());
+
+// var sonuc = (sayi>0) ? "sayı pozitif":"sayı negatif";
+// Console.WriteLine(sonuc);
+
+// Console.Write("Sayı:");
+// var sayi = Convert.ToInt32(Console.ReadLine());
+// var sonuc = (sayi%2 == 0) ? "çift":"tek";
+// Console.WriteLine(sonuc);
---
+++ Operatorler/MantiksalOperatorler.cs
@@ -0,0 +1,36 @@
+/*
+
+MANTIKSAL OPERATÖRLER
+
+string username = "dafi";
+string password = "12345"
+
+bool valid_username = (username == "dafi");
+bool valid_password = (password == "12345");
+
+valid_username => true
+valid_password => true
+ise giriş yapılabilmeli
+
+
+&& VE
+|| VEYA
+! DEĞİL
+
+*/
+var a = true;
+var b = true;
+var c = false;
+var d = false;
+
+var sonuc = (a&&b); // true
+var sonuc1 = (a&&c);    //false
+var sonuc2 = (c&&d);    //false
+Console.WriteLine(sonuc1);
+
+var sonuc3 = (a||b);    //true
+var sonuc4 = (a||c);    //true
+var sonuc5 = (c||d);    //false
+
+var sonuc6 = !c;
+Console.WriteLine(sonuc6);
