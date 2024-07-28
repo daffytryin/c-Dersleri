9e197e1789ae3fcb3e3f5630fae4c53e983fbe3e
denizokan.taspinar <denizokan.taspina@arcelik.com>
2024-07-28 21:44:23 +0300

    operatorler master branchinden kaldırıldı

---
+++ StringAndDates/DateTime.cs
@@ -0,0 +1,25 @@
+/*
+DateTime
+*/
+
+// using System.Data.Common;
+
+// DateTime simdi = DateTime.Now;
+
+// Console.WriteLine(simdi);
+// Console.WriteLine(simdi.Year);
+// Console.WriteLine(simdi.Month);
+// Console.WriteLine(simdi.Day);
+// Console.WriteLine(simdi.DayOfWeek);
+
+
+// DateTime dt = new DateTime(2009,5,10,12,5,3);
+
+// Console.WriteLine(dt);
+
+// DateTime dt2 = dt.AddYears(5);
+// Console.WriteLine(dt2);
+
+// var fark = simdi - dt2;
+// Console.WriteLine(fark.TotalDays); //aradaki farkın toplam gün sayısı
+// Console.WriteLine(fark.TotalMinutes);
